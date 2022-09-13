ec53d127c22bd6bb4691fc2f38449991ef58ba5e
Daron Wiafe <daronkwiafe@gmail.com>
2022-09-13 22:49:14 +0200

    Complete mobile section, home and about

--- script.js
+++ script.js
@@ -1,58 +1,142 @@
-// conference speakers
+// conference speakers data
+
 const speaker = [
     {
         name: 'Yvonne Aki-Sawyerr',
         title: 'Mayor of Freetown',
-        info: 'Yvonne Denise Aki-Sawyerr, OBE is a Sierra Leonean politician and finance professional, who is serving as the current mayor of Freetown, Sierra Leone\'s capital and largest city; she assumed office on 11 May 2018',
+        info: 'OBE is a Sierra Leonean politician, who is serving as the current mayor of Freetown',
         image: 'images/speaker1.jpg',
         imageText: 'Yvonne Aki-Sawyerr'
     },
     {
         name: 'Brodie Boland',
         title: 'Partner, McKinsey & Company',
-        info: 'Helps leading real-estate organizations, infrastructure investors, and city governments prepare for the future, including by identifying and mitigating the risks from climate change',
+        info: 'Brodie is a leader of McKinsey\'s work on climate risk in the real-estate and infrastructure sectors',
         image: 'images/speaker2.jpg',
         imageText: 'Brodie Boland'
     },
     {
         name: 'Nathalie Palladitcheff',
         title: 'President and CEO, Ivanhoé Cambridge',
-        info: 'President and the first female Chief Executive Officer of Ivanhoé Cambridge, the real estate subsidiary of the Caisse de dépôt et placement du Québec, which owns C$60 billion in real estate assets.',
-        image: 'images/speaker3',
+        info: 'President of Ivanhoé Cambridge, the real estate subsidiary of the Caisse de dépôt et placement du Québec',
+        image: 'images/speaker3.jpg',
         imageText: 'Nathalie Palladitcheff'
     },
     {
         name: 'Bill Winters',
         title: 'Chief Executive Officer of Standard Chartered',
-        info: 'William Thomas Winters, CBE is an American banker who is the chief executive of Standard Chartered, and was formerly co-head of JPMorgan Chase\'s investment bank',
-        image: 'images/speaker4',
+        info: 'CBE is an American banker who is the chief executive of Standard Chartered, and was formerly co-head of JPMorgan Chase\'s investment bank',
+        image: 'images/speaker4.jpg',
         imageText: 'Bill Winters'
     },
     {
         name: 'Divya Seshamani',
         title: 'Managing Partner, Greensphere',
-        info: 'As Founder and Managing Partner of Greensphere Capital, she manages funds that invest in sustainable technology, companies and projects in areas such as sustainable forestry, agriculture, renewable energy, resilient infrastructure, water, resource and energy efficiency.',
-        image: 'images/speaker5',
+        info: 'She manages funds that invest in sustainable technology and projects in areas such as sustainable forestry, and energy efficiency',
+        image: 'images/speaker5.jpg',
         imageText: 'Divya Seshamani'
     },
     {
         name: 'Virginia Simmons',
         title: 'Managing Partner, UK and Ireland, McKinsey & Company',
-        info: 'Virginia brings deep experience in supporting retailers, banks, and other customer-facing firms to drive growth, harness new technologies, and transform marketing and service operations',
-        image: 'images/speaker6',
+        info: 'Virginia brings experience in supporting retailers and banks to drive growth and harness new technologies',
+        image: 'images/speaker6.jpg',
         imageText: 'Virginia Simmons'
     }
 ]
 
-const speakerImg = document.querySelectorAll('speaker-img');
-const speakerName = document.querySelectorAll('speaker-name');
-const speakerPosition = document.querySelectorAll('speaker-position');
-const speakerStory = document.querySelectorAll('speaker-story');
+// conference speakers dynamic html
 
-for (let i = 0; i < 6; i += 1) {
-    speakerImg[i].setAttribute('src', speaker[i].image);
-    speakerImg[i].setAttribute('alt', speaker[i].imageText);
-    speakerName[i].textContent = speaker[i].name;
-    speakerPosition[i].textContent = speaker[i].title;
-    speakerStory[i].textContent = speaker[i].info;
+const speakerContainerFirstHalf = document.createElement('div');
+const speakerContainerSecondHalf = document.createElement('div');
+
+speakerContainerFirstHalf.classList.add('speaker-half-one-container');
+speakerContainerSecondHalf.classList.add('speaker-half-one-container');
+speakerContainerSecondHalf.classList.add('display-none');
+
+for (let i = 0; i < 3; i++) {
+    
+    const speakerContainer = document.querySelector('.speakers');
+    const speakerList = document.createElement('div');
+    const speakerImg = document.createElement('img');
+    const speakerInfo = document.createElement('div');
+    const speakerName = document.createElement('h4');
+    const speakerPosition = document.createElement('p');
+    const speakerStory = document.createElement('p');
+
+    speakerList.classList.add('speaker-list');
+    speakerImg.classList.add('speaker-img');
+    speakerInfo.classList.add('speaker-info');
+    speakerName.classList.add('speaker-name');
+    speakerStory.classList.add('speaker-story');
+    speakerPosition.classList.add('speaker-position');
+    speakerStory.classList.add('speaker-story');
+
+    speakerInfo.appendChild(speakerName);
+    speakerInfo.appendChild(speakerPosition);
+    speakerInfo.appendChild(speakerStory);
+    speakerList.appendChild(speakerImg);
+    speakerList.appendChild(speakerInfo);
+
+    speakerImg.setAttribute('src', speaker[i].image);
+    speakerImg.setAttribute('alt', speaker[i].imageText);
+    speakerName.textContent = speaker[i].name;
+    speakerPosition.textContent = speaker[i].title;
+    speakerStory.textContent = speaker[i].info;
+
+    speakerContainerFirstHalf.appendChild(speakerList);
+    speakerContainer.appendChild(speakerContainerFirstHalf)
 }
+
+for (let i = 3; i < 6; i++) {
+    
+    const speakerContainer = document.querySelector('.speakers');
+    const speakerList = document.createElement('div');
+    const speakerImg = document.createElement('img');
+    const speakerInfo = document.createElement('div');
+    const speakerName = document.createElement('h4');
+    const speakerPosition = document.createElement('p');
+    const speakerStory = document.createElement('p');
+
+    speakerList.classList.add('speaker-list');
+    speakerImg.classList.add('speaker-img');
+    speakerInfo.classList.add('speaker-info');
+    speakerName.classList.add('speaker-name');
+    speakerStory.classList.add('speaker-story');
+    speakerPosition.classList.add('speaker-position');
+    speakerStory.classList.add('speaker-story');
+
+    speakerInfo.appendChild(speakerName);
+    speakerInfo.appendChild(speakerPosition);
+    speakerInfo.appendChild(speakerStory);
+    speakerList.appendChild(speakerImg);
+    speakerList.appendChild(speakerInfo);
+
+    speakerImg.setAttribute('src', speaker[i].image);
+    speakerImg.setAttribute('alt', speaker[i].imageText);
+    speakerName.textContent = speaker[i].name;
+    speakerPosition.textContent = speaker[i].title;
+    speakerStory.textContent = speaker[i].info;
+
+    speakerContainerSecondHalf.appendChild(speakerList);
+    speakerContainer.appendChild(speakerContainerSecondHalf)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
